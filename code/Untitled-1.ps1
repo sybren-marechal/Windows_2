40de55f027d4ae12410d081d3832b3a0544db3d4
sybren marechal <sybrenmarechal@icloud.com>
2018-10-02 01:05:46 +0200

    working with manager X

--- code/Untitled-1.ps1
+++ code/Untitled-1.ps1
@@ -5,11 +5,20 @@ $logfile = ".\Documents\NEW_USER\logfile.txt"
 $date = Get-Date
 
 
+$users = import-csv $csvfile
 
-import-csv $csvfile | ForEach-Object {
-New-ADUser -Name $_.Voornaam -OtherAttributes @{'mail'=$_.voornaam + "." + $_.naam + "@fabrikam.com"; UserPrincipalName= $_.account}
-add-ADGroupMember -Identity $_.groupname -Members $_.voornaam
-Move-ADobject (get-aduser $_.voornaam).DistinguishedName -TargetPath "OU=verkoop,OU=PFafdeling,DC=PoliFormaSM,DC=be";
+
+ForEach ($item in $users ) {
+    if ($item.Manager.Equals("X")) {
+        $ou = "verkoop"
+    }
 }
 
-##########
+write $ou
+
+$users | ForEach-Object {
+    New-ADUser -Name $_.Voornaam -OtherAttributes @{'mail' = $_.voornaam + "." + $_.naam + "@fabrikam.com"; UserPrincipalName = $_.account}
+    add-ADGroupMember -Identity $_.groupname -Members $_.voornaam
+    Move-ADobject (get-aduser $_.voornaam).DistinguishedName -TargetPath "OU=$OU,OU=PFafdeling,DC=PoliFormaSM,DC=be";
+}
+
