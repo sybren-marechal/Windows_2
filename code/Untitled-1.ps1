0fa721f136d7da4d185b1c3af815f22db08a2fa8
sybren marechal <sybrenmarechal@icloud.com>
2018-10-02 00:08:50 +0200

    working import and relocate user

--- code/Untitled-1.ps1
+++ code/Untitled-1.ps1
@@ -1,31 +1,15 @@
 
-MAKE USER
 
-import-csv ".\Documents\NEW_USER\groups.csv" | ForEach-Object {
-    New-ADUser -Name $_.voornaam -OtherAttributes @{'title'="director";'mail'="chewdavid@fabrikam.com"}
-} 
-
-
-ADD USER TO GROOP
-
-import-csv ".\Documents\NEW_USER\groups.csv" | ForEach-Object {
-    add-ADGroupMember -Identity $_.groupname -Members $_.voornaam
-}
+$csvfile = ".\Documents\NEW_USER\groups.csv"
+$logfile = ".\Documents\NEW_USER\logfile.txt"
+$date = Get-Date
 
 
 
-MOVE USER
-
-import-csv ".\Documents\NEW_USER\groups.csv" | ForEach-Object {
-    Move-ADobject (get-aduser $_.voornaam).DistinguishedName -TargetPath 'OU=verkoop,OU=PFafdeling,DC=PoliFormaSM,DC=be';
-}
-
-ALLL
-
-import-csv ".\Documents\NEW_USER\groups.csv" | ForEach-Object {
-    New-ADUser -Name $_.voornaam -OtherAttributes @{'SamAccountName'= $_.Account ;'name'=$_.voornaam; 'surname'=$_.naam;'mail'=$_.voornaam+"."+$_.naam+"@fabrikam.com"}
-    add-ADGroupMember -Identity $_.groupname -Members $_.voornaam
-    Move-ADobject (get-aduser $_.voornaam).DistinguishedName -TargetPath 'OU=verkoop,OU=PFafdeling,DC=PoliFormaSM,DC=be';
+import-csv $csvfile | ForEach-Object {
+New-ADUser -Name $_.Voornaam -OtherAttributes @{'mail'=$_.voornaam + "." + $_.naam + "@fabrikam.com"; UserPrincipalName= $_.account}
+add-ADGroupMember -Identity $_.groupname -Members $_.voornaam
+Move-ADobject (get-aduser $_.voornaam).DistinguishedName -TargetPath "OU=verkoop,OU=PFafdeling,DC=PoliFormaSM,DC=be";
 }
 
 ##########
