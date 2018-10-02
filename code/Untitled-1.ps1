02f156a1de7ddc3f47a530281f94adf7e9d0e914
sybren marechal <sybrenmarechal@icloud.com>
2018-10-02 11:07:40 +0200

    working with variables

--- code/Untitled-1.ps1
+++ code/Untitled-1.ps1
@@ -4,38 +4,47 @@ $csvfile = ".\Documents\NEW_USER\groups.csv"
 $logfile = ".\Documents\NEW_USER\logfile.txt"
 $date = Get-Date
 
+##########################################
+
 $users = import-csv $csvfile
 $domain_controller = "PoliformaSM"
 $ou_afdeling = "PFafdeling"
-$GivenName = $_.Voornaam 
-$Surname = $_.naam
 
-
+##########################################
 
 ForEach ($item in $users ) {
     if ($item.Manager.Equals("X")) {
-        $ou = "verkoop"
+        $ou = "verkoop" #manager
+        $group = "verkoop" #manager
     }
     if ($item.IT.Equals("X")) {
         $ou = "it"
+        $group = "it"
     }
     if ($item.boekhouding.Equals("X")) {
         $ou = "boekhouding"
+        $group = "boekhouding"
     }
     if ($item.logistiek.Equals("X")) {
         $ou = "logistiek"
+        $group = "logistiek"
     }
     if ($item.importExport.Equals("X")) {
         $ou = "importExport"
+        $group = "importExport"
     }
 }
 
 
-write $ou
-
 $users | ForEach-Object {
-    New-ADUser -Name $_.Voornaam  -OtherAttributes @{'mail' = $_.voornaam + "." + $_.naam + "@fabrikam.com"; UserPrincipalName = $_.account}
-    add-ADGroupMember -Identity $_.groupname -Members $_.voornaam
-    Move-ADobject (get-aduser $_.voornaam).DistinguishedName -TargetPath "OU=$OU,OU=$ou_afdeling,DC=$domain_controller,DC=be";
+
+    $GivenName = $_.Voornaam
+    $Surname = $_.naam
+    $accountName =  $_.account
+    $sam = $GivenName + "." + $Surname
+
+    New-ADUser -Name $sam  -OtherAttributes @{'mail' = $GivenName + "." + $Surname + "@fabrikam.com"; UserPrincipalName = $accountName}
+    add-ADGroupMember -Identity $group -Members $sam
+    Move-ADobject (get-aduser $sam).DistinguishedName -TargetPath "OU=$OU,OU=$ou_afdeling,DC=$domain_controller,DC=be";
 }
 
