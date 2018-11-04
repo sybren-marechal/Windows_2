ef06ea465be188f3110c6aa0e8231b2face699b0
sybren marechal <sybrenmarechal@icloud.com>
2018-11-04 23:21:08 +0100

    created a try-catsh around create user and add some output to console. Alsow changes de names of de groups for testing

--- code/Untitled-1.ps1
+++ code/Untitled-1.ps1
@@ -1,10 +1,15 @@
+#https://social.technet.microsoft.com/wiki/contents/articles/24541.powershell-bulk-create-ad-users-from-csv-file.aspx
+
 
 
 $csvfile = ".\Documents\NEW_USER\groups.csv"
 $logfile = ".\Documents\NEW_USER\logfile.txt"
 $date = Get-Date
 
-##########################################
+
+#----------------------------------------------------------
+#STATIC VARIABLES
+#----------------------------------------------------------
 
 $users = import-csv $csvfile
 $domain_controller = "PoliformaSM"
@@ -18,19 +23,19 @@ ForEach ($item in $users ) {
         $group = "verkoop" #manager
     }
     if ($item.IT.Equals("X")) {
-        $ou = "it"
-        $group = "it"
+        $ou = "staf" #it
+        $group = "staf"
     }
     if ($item.boekhouding.Equals("X")) {
-        $ou = "boekhouding"
-        $group = "boekhouding"
+        $ou = "Directie" #boekhouding 
+        $group = "Directie"
     }
     if ($item.logistiek.Equals("X")) {
-        $ou = "logistiek"
-        $group = "logistiek"
+        $ou = "productie" #logistiek
+        $group = "productie"
     }
     if ($item.importExport.Equals("X")) {
-        $ou = "importExport"
+        $ou = "importExport" # importExport
         $group = "importExport"
     }
 }
@@ -43,8 +48,18 @@ $users | ForEach-Object {
     $accountName =  $_.account
     $sam = $GivenName + "." + $Surname
 
-    New-ADUser -Name $sam  -OtherAttributes @{'mail' = $GivenName + "." + $Surname + "@fabrikam.com"; UserPrincipalName = $accountName}
+    try {
+        New-ADUser -Name $sam  -OtherAttributes @{'mail' = $GivenName + "." + $Surname + "@fabrikam.com"; UserPrincipalName = $accountName}
+    write-output "created user $sam" 
+    }
+    catch {
+        write-output "user $sam already exist" 
+    }
+
     add-ADGroupMember -Identity $group -Members $sam
+    write-output "add $sam to the group $group" 
     Move-ADobject (get-aduser $sam).DistinguishedName -TargetPath "OU=$OU,OU=$ou_afdeling,DC=$domain_controller,DC=be";
+    write-output "move $sam to the OU= $ou and the afdeling= $ou_afdeling " 
+
 }
 
